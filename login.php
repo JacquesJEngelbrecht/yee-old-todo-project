0a95af2a241d17a7a648a1b6f1ac635cf7da3335
JacquesEngelbrecht <jacquesjengelbrecht@gmail.com>
2020-11-28 12:18:21 +0200

    Yee Navbar added

--- login.php
+++ login.php
@@ -11,24 +11,35 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
     <body>
+    <?php $page = 'home'; ?>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link <?php echo ($page == 'home') ? 'active':''; ?>" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo ($page == 'todo1') ? 'active':''; ?>" href="">Todo1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo ($page == 'todo2') ? 'active':''; ?>" href="">Todo2</a>
+                </li>
+                </ul>
+            </div>
+            <div class="float-left">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
     </body>
 </html>
